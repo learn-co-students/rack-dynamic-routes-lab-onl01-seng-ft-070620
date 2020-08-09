005bdcc4acadacb05cb3702a5ec5940ebec6f1db
Doug Schallmoser <dougschallmoser@mgail.com>
2020-08-08 22:34:10 -0700

    Done.

--- app/application.rb
+++ app/application.rb
@@ -10,7 +10,7 @@ class Application
         if req.path.match(/items/)
             item_name = req.path.split("/items/").last
             if item = @@items.detect {|item| item.name == item_name}
-                resp.write "#{item.price}"
+                resp.write item.price
             else 
                 resp.status = 400
                 resp.write "Item not found"
