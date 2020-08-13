cce993d2b1c0f0fab7a617289a3b4b6d95de8d13
Ethan <eleetyson@gmail.com>
2020-08-13 09:25:53 -0400

    Done.

--- app/application.rb
+++ app/application.rb
@@ -9,15 +9,16 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
 
+      # if the item is found, return its price
       if @@items.find { |i| i.name == item_name}
         item = @@items.find { |i| i.name == item_name}
         resp.write item.price
-      else
+      else # if it's not found, return a 400 error
         resp.write "Item not found"
         resp.status = 400
       end
 
-    else
+    else # if the user used an invalid path, return a 404 error
       resp.write "Route not found"
       resp.status = 404
     end
